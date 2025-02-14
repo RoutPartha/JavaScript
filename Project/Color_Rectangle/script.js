35e40ff0521d8ab99a8f370ef1f3dc599174a301
RoutPartha <parthasarathirout408@gmail.com>
2025-02-14 23:59:44 +0530

    Added some changes

--- Project/Color_Rectangle/script.js
+++ Project/Color_Rectangle/script.js
@@ -8,13 +8,13 @@ rect.addEventListener("mousemove", function (event) {
         var redColor = gsap.utils.mapRange(0, rectanglelocation.width / 2, 255, 0, insiderectangle);
         gsap.to(rect, {
             backgroundColor: `rgb(${redColor}, 0, 0)`,
-            ease: Power4,
+            // ease: Power4,
         });
     } else {
         var blueColor = gsap.utils.mapRange(rectanglelocation.width / 2, rectanglelocation.width, 0, 255, insiderectangle);
         gsap.to(rect, {
             backgroundColor: `rgb(0, 0, ${blueColor})`,
-            ease: Power4,
+            // ease: Power4,
         });
     }
 });
